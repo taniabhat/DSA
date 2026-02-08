ef05313f7f851e47e098f04197ebd00f7b104da6
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2026-02-08 23:23:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (65%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int rob1(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=prev2;
+
+            int nottake=0+ prev;
+
+            int curri=max(take, nottake);
+
+            prev2=prev;
+            prev=curri;
+        }
+        return prev;
+    }
+    int rob(vector<int>& nums) {
+        vector<int> temp1, temp2;
+        int n=nums.size();
+        if(n==1) return nums[0];
+        for(int i=0;i<n;i++){
+            if(i != 0) temp1.push_back(nums[i]);
+            if(i != n-1) temp2.push_back(nums[i]);
+        }
+        return max(rob1(temp1), rob1(temp2));
+    }
+};
