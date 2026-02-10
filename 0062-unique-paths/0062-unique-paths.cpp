ea314325cf51d771ec7074f1257d40147bde8bdf
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2026-02-10 20:47:04 +0530

    Time: 0 ms (100%), Space: 8 MB (88.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int N=n+m-2;
-        int r=m-1;
+        int r=n-1;
         double res=1;
         for(int i=1;i<=r;i++){
             res=res*(N-r+i)/i;
