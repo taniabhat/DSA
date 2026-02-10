a294780cf60eaae0e1b9e21f8a2db12f686a383c
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2026-02-10 20:26:10 +0530

    Time: 3 ms (9.3%), Space: 9.3 MB (52.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int rec(int i, int j){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=rec(i-1,j);
+        int left=rec(i,j-1);
+        return dp[i][j]=left+up;
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-            }
-        }
-        return dp[m - 1][n - 1];
+        dp.assign(m,vector<int> (n,-1));
+        return rec(m-1,n-1);
     }
 };
