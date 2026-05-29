db010d741f226e94f2247d098609b619c0d46888
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2026-05-29 08:47:13 +0530

    Time: 59 ms (56.01%), Space: 21 MB (70.21%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!q.empty()){
+            string word=q.front().first;
+            int steps=q.front().second;
+            q.pop();
+            if(word==endWord) return steps;
+            for(int i=0;i<word.size();i++){
+                char ori=word[i];
+                for(char c='a'; c<='z';c++){
+                    word[i]=c;
+                    //means it exists
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        q.push({word,steps+1});
+                    }
+                }
+                word[i]=ori;
+            }
+        }
+        return 0;
+    }
+};
