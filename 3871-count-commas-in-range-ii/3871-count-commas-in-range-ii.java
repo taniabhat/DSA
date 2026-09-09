97e17299a70126cb51537ed58d7a0f9dfa60de10
Tania Bhattacharyya <147084504+taniabhat@users.noreply.github.com>
2026-09-09 20:55:20 +0530

    Time: 1 ms (99.36%), Space: 42.6 MB (56.41%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long countCommas(long n) {
+        long start=1000;
+        long commas=1;
+        long ans=0;
+        while(start<=n){
+            long end=start*1000-1;
+
+            if(end>n){
+                end=n;
+            }
+
+            ans+=(end-start+1)*commas;
+
+            start*=1000;
+            commas++;
+        }
+        return ans;
+    }
+}
